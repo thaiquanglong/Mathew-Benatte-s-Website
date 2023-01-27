530eb5c20d9dfbf0d1b3d1e5a6c927b68df444a9
Long Thai <thaiquanglong2001@gmail.com>
2023-01-27 10:23:39 -0800

    spring boot initializer

---
+++ matthew_business_website/src/main/java/com/mathew_real_eastate/matthew_business_website/MatthewBusinessWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.mathew_real_eastate.matthew_business_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatthewBusinessWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatthewBusinessWebsiteApplication.class, args);
+	}
+
+}
---
+++ matthew_business_website/src/test/java/com/mathew_real_eastate/matthew_business_website/MatthewBusinessWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathew_real_eastate.matthew_business_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatthewBusinessWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
