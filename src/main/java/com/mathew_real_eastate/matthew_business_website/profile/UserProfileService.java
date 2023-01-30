b4e2bcf498e73f1fc2242677a6ae6f8322de6d0e
Long Thai <thaiquanglong2001@gmail.com>
2023-01-30 05:19:11 -0800

    server online

--- src/main/java/com/mathew_real_eastate/matthew_business_website/profile/UserProfileService.java
+++ src/main/java/com/mathew_real_eastate/matthew_business_website/profile/UserProfileService.java
@@ -1,8 +1,10 @@
 package com.mathew_real_eastate.matthew_business_website.profile;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,7 +23,13 @@ public class UserProfileService {
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
         // check wether image is empty
+        if( file.isEmpty()){
+            throw new IllegalStateException("Cannot upload empty file [ " + file.getSize() + "]");
+        }
         // check if file is an image
+        if(!Arrays.asList(ContentType.IMAGE_JPEG, ContentType.IMAGE_PNG, ContentType.IMAGE_GIF).contains(file.getContentType())){
+            throw new IllegalStateException("File must be an image");
+        }
         // check the use exist
         // grab meetadata
         // store the image in s3 and update database with s3 image link
